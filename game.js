f5cabba14ad720ddf48df1f8fc5a258d9929158e
Isaac Korankye Asante <xquisit31@gmail.com>
2024-04-24 19:46:24 +0000

    display hard coded questions

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -43,16 +43,35 @@ const MAX_QUESTIONS = 3;
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
+     availableQuestions = [...questions];
     getNewQuestion();
 }
 
 getNewQuestion = () => {
+    if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
+        return window.location.assign("/end.html");
+    }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
 
 }
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if(!acceptingAnswers) return;
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+        getNewQuestion();
+    });
+});
 
 startGame();
