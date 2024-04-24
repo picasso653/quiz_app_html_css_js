26cfb6d58990e49a7d0c44bbd8e477ab1f33af0a
Isaac Korankye Asante <xquisit31@gmail.com>
2024-04-24 20:22:57 +0000

    head's up display

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -52,9 +53,13 @@ getNewQuestion = () => {
         return window.location.assign("/end.html");
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
+
     choices.forEach(choice => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
@@ -74,6 +79,11 @@ choices.forEach(choice => {
   
       const classToApply =
         selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        if (classToApply === "correct") {
+            incrementScore(CORRECT_BONUS);
+          }
+      
   
       selectedChoice.parentElement.classList.add(classToApply);
   
@@ -83,5 +93,10 @@ choices.forEach(choice => {
       }, 1000);
     });
 });
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+  };
 
 startGame();
